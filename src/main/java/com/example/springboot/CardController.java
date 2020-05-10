e2ab38c8e048fe04a1a99b0c6c6319f48f374f7f
mentathiel <darinkazobenica@gmail.com>
2020-05-10 22:19:26 +0200

    cards

--- src/main/java/com/example/springboot/CardController.java
+++ src/main/java/com/example/springboot/CardController.java
@@ -9,18 +9,47 @@ public class CardController {
 
     @GetMapping("/")
     public String index(Model model){
-        String prompt = "exampleprompt";
-        model.addAttribute("prompt", prompt);
-        model.addAttribute("card1", "examplecard1");
-        model.addAttribute("card2", "examplecard2");
-        model.addAttribute("card3", "examplecard3");
+        CardTextGenerator cg = new CardTextGenerator();
+        model.addAttribute("prompt", cg.getPrompt());
+        model.addAttribute("card1", cg.getCard());
+        model.addAttribute("card2", cg.getCard());
+        model.addAttribute("card3", cg.getCard());
+        model.addAttribute("card4", cg.getCard());
         return "index";
     }
 
-//    @GetMapping("/")
-//    public String index(@RequestParam(name="prompt", required=false, defaultValue="World") String prompt, Model model) {
-//        model.addAttribute("prompt", prompt);
-//        return "index";
-//    }
+    @GetMapping("/black")
+    public String black(Model model){
+        CardTextGenerator cg = new CardTextGenerator();
+        model.addAttribute("black", cg.getAllPrompts());
+        return "black";
+    }
 
+    @PostMapping("/black")
+    public String addBlack(@RequestParam(name="prompt", required=false, defaultValue="") String prompt, Model model){
+        if(prompt != "") {
+            CardTextGenerator cg = new CardTextGenerator();
+            cg.addPrompt(prompt);
+            model.addAttribute("black", cg.getAllPrompts());
+        }
+        return "black";
+    }
+
+    @GetMapping("/white")
+    public String white(Model model){
+        CardTextGenerator cg = new CardTextGenerator();
+        model.addAttribute("white", cg.getAllCards());
+
+        return "white";
+    }
+
+    @PostMapping("/white")
+    public String white(@RequestParam(name="card", required=false, defaultValue="") String card, Model model){
+        if(card != "") {
+            CardTextGenerator cg = new CardTextGenerator();
+            cg.addCard(card);
+            model.addAttribute("white", cg.getAllCards());
+        }
+        return "white";
+    }
 }
--- src/main/java/com/example/springboot/CardTextGenerator.java
+++ src/main/java/com/example/springboot/CardTextGenerator.java
@@ -1,4 +1,90 @@
 package com.example.springboot;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTextGenerator {
+    private LinkedList<String> prompts;
+    private LinkedList<String> cards;
+
+    public CardTextGenerator(){
+        prompts = new LinkedList<String>();
+        cards = new LinkedList<String>();
+
+        File promptFile = new File("src/main/resources/static/prompt.txt");
+        File cardFile = new File("src/main/resources/static/card.txt");
+
+        try {
+            Scanner scanner = new Scanner(promptFile);
+            while (scanner.hasNextLine()) {
+                prompts.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Scanner scanner = new Scanner(cardFile);
+            while (scanner.hasNextLine()) {
+                cards.add(scanner.nextLine());
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getPrompt(){
+        Random rand = new Random();
+        return prompts.get(rand.nextInt(prompts.size()));
+    }
+
+    public String getCard(){
+        Random rand = new Random();
+        return cards.get(rand.nextInt(cards.size()));
+    }
+
+    public String getAllPrompts(){
+        StringBuilder sb = new StringBuilder();
+        for(String s : prompts) {
+            sb.append(s+"<br/>");
+        }
+        return sb.toString();
+    }
+
+    public String getAllCards(){
+        StringBuilder sb = new StringBuilder();
+        for(String s : cards) {
+            sb.append(s+"<br/>");
+        }
+        return sb.toString();
+    }
+
+    public void addPrompt(String prompt) {
+        File promptFile = new File("src/main/resources/static/prompt.txt");
+
+        try(FileWriter fr = new FileWriter(promptFile, true)) {
+            fr.write("\n" + prompt);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        prompts.add(prompt);
+    }
+
+    public void addCard(String card) {
+        File cardFile = new File("src/main/resources/static/card.txt");
+
+        try(FileWriter fr = new FileWriter(cardFile, true)) {
+            fr.write("\n" + card);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        prompts.add(card);
+    }
 }
